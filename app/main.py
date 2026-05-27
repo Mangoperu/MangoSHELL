03ba65e293a55ae4abf16c4638b7fed58b89ab57
deepak0642 <deepakyadav0642@gmail.com>
2026-05-27 19:49:14 +0530

    added cd command to change directory and pwd to print current working directory also added handling "" and ''

--- app/main.py
+++ app/main.py
@@ -1,14 +1,16 @@
 import os
 import sys
+import subprocess
+import shlex
 
-def getpath(cd): #to check for executable file in PATH variables
+def getpath(cmnd): #to check for executable file in PATH variables
 
     totalpossiblepaths = os.environ.get("PATH" , "") #getting total available paths that i can access
     if not totalpossiblepaths :
         return None
     directories = totalpossiblepaths.split(os.pathsep)  # os.pathsep  and os.pathjoin seperates and joins by using : , ; depending on Mac and Windows
     for i in directories :
-        full_path = os.path.join(i , cd)
+        full_path = os.path.join(i , cmnd)
         if os.path.isfile(full_path) and os.access(full_path, os.X_OK): # this will check if this file exist in that place or is executable
             return full_path
     return None
@@ -17,6 +19,22 @@ def getpath(cd): #to check for executable file in PATH variables
 
 def run_exit():
     sys.exit(0)
+
+
+
+def run_cwd(arg):
+    print(os.getcwd())
+
+def run_cd(args):
+    arg = args[0]
+    complete_path = os.path.expanduser(arg)
+    try :
+        os.chdir(complete_path)
+    except:
+        print(f"cd: {arg}: No such file or directory")
+
+
+
 
 def run_echo(args):
     print(" ".join(args)) # i used loop before but print() command adds new line character so have to use this one..
@@ -35,7 +53,9 @@ def run_type(args , comms ):
 def main():
     comms = { "exit": run_exit
              , "type" : run_type 
-             , "echo" : run_echo }
+             , "echo" : run_echo 
+             , "pwd" : run_cwd
+             , "cd" : run_cd}
    
     while(True) :
         sys.stdout.write("$ ")
@@ -43,19 +63,23 @@ def main():
         command = input().strip()
         if not command:
             continue
-        parts = command.split()
-        cd = parts[0]
+        parts = shlex.split(command)
+        cmnd = parts[0]
         args = parts[1:]
 
-        if cd in comms:
-            if cd == "type":
+        if cmnd in comms:
+            if cmnd == "type":
                 comms["type"](args, comms)
-            elif cd == "exit":
+            elif cmnd == "exit":
                 comms["exit"]() 
             else:
-                comms[cd](args)
+                comms[cmnd](args)
         else:
-            print(f"{command}: command not found")
+
+            if getpath(cmnd) :
+                subprocess.run(parts, executable=getpath(cmnd))
+            else:
+                print(f"{command}: command not found")
         
         
 
